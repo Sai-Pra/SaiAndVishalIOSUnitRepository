8691a60118a50da71a1223f4bb9e3d00848cff39
Saipranav Venkatakrishnan <saipranavvk@gmail.com>
2021-12-02 11:03:46 -0600

    Progress 12/02/2021

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by Saipranav Venkatakrishnan on 12/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view._
+    }
+
+
+}
+
