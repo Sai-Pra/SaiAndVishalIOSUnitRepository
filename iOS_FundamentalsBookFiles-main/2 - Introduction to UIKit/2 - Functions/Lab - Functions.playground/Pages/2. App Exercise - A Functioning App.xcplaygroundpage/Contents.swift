11df2081e18fd9ea6e4e83f81b6de7e6ba94ca9a
VishalKaushik123 <vishakau6@gmail.com>
2021-12-08 11:00:30 -0600

    Did some parts of string lab

--- iOS_FundamentalsBookFiles-main/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ iOS_FundamentalsBookFiles-main/2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -8,11 +8,29 @@
  A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
  */
 var steps = 0
-
+func incrementSteps(){
+    steps+=1;
+}
+incrementSteps()
+incrementSteps()
+incrementSteps()
+print(steps)
 //:  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout.
-let goal = 10000 
+let goal = 10000
+let ten = 1000
+let fifty = 5000
+let ninty = 9000
 
 
+func progressUpdate(){
+    if(steps < ten){
+        print("You're off to a good start.")
+    }else if(steps < fifty){
+        print("You're almost halfway there!")
+    }else if(steps < ninty){
+        print("You're almost there!")
+    }
+}
 /*:
 [Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
  */
